70d9777b4e3a5c969233f2deaf03a1ff50adc02a
Neha Gupta <8225994+bugbugger@users.noreply.github.com>
2018-10-19 16:15:20 +0530

    Update summarization.py

--- summarization.py
+++ summarization.py
@@ -42,7 +42,6 @@ def dataPreprocessing(sentence):
 	stemmedData = stemming(filteredData)
 	return stemmedData
 
-
 def extractFeature(sentTFIDF,istitle,sentLabel):
 	global sentFeature
 	for sentId,scores in sentTFIDF.items():
@@ -89,7 +88,6 @@ def splitSentLabel(data,title):
 
 def calcFeatureProb(totalSentences):
 	featureProb = defaultdict(lambda: defaultdict(lambda: int))
-
 	probLabel0 = labelFreq[0]/totalSentences
 	probLabel1 = labelFreq[1]/totalSentences
 	probLabel2 = labelFreq[2]/totalSentences
@@ -100,16 +98,6 @@ def calcFeatureProb(totalSentences):
 			featureProb[sLabel][d] = freq/float(noOfentry)
 
 	return featureProb,probLabel0,probLabel1,probLabel2
-
-def naiveBayesClassifier(featureProb,probLabel0,probLabel1,probLabel2):
-	predictedProb = {}
-	for sentId,data in sentFeature.items():
-		prob0 = probLabel0 * featureProb[0][data[0]] * featureProb[0][data[1]]
-		prob1 = probLabel1 * featureProb[1][data[0]] * featureProb[1][data[1]]
-		prob2 = probLabel2 * featureProb[2][data[0]] * featureProb[2][data[1]]
-		predictedProb[sentId] = max(prob0,prob1,prob2)
-	return predictedProb
-
 
 def traindata():
 	path = "summarizationdataset/dailymail/training/"
@@ -132,18 +120,70 @@ def traindata():
 		sentTFIDF = calcTFIDF(vocabWordFreq,totalSentences,sentLabel)
 		extractFeature(sentTFIDF,istitle,sentLabel)			
 		articleSummary = data[2]
+		entityMapping = data[3]
+	featureProb,probLabel0,probLabel1,probLabel2 = calcFeatureProb(totalSentences)
+	return featureProb,probLabel0,probLabel1,probLabel2
+
+
+accuracy = 0
+def checkAccuracy(predictedLabel,sentLabel,totalSentences):
+	global accuracy
+	count = 0
+	for sentId,label in predictedLabel.items():
+		if label == sentLabel[sentId]:
+			count += 1
+
+	accuracy += count/float(totalSentences)
+
+
+def naiveBayesClassifier(testSentFeature,featureProb,probLabel0,probLabel1,probLabel2):
+	predictedProb = {}
+	for sentId,data in testSentFeature.items():
+		for d in data:
+			probLabel0 =* featureProb[0][d[0]] * featureProb[0][d[1]]
+			probLabel1 =* featureProb[1][d[0]] * featureProb[1][d[1]]
+			probLabel2 =* featureProb[2][d[0]] * featureProb[2][d[1]]
+		predictedProb[sentId] = max(probLabel0,probLabel1,probLabel2)
+	return predictedProb
+
+def extractTestFeature(sentTFIDF,istitle):
+	sentFeature = defaultdict(lambda: list())
+	for sentId,scores in sentTFIDF.items():
+		avgTFIDF = math.floor(scores[0]/float(scores[1]))
+		freqTitle = istitle[sentId]
+		sentFeature[sentId].append([avgTFIDF,freqTitle])		
+	return sentFeature
+
+def testdata():
+	path = "summarizationdataset/dailymail/testing/"
+	totalSentences = 0
+	for articleName in listdir(path):
+		filename = path + articleName
+		file = open(filename,encoding='utf-8')
+		data = file.read()
+		file.close()
+		data = re.split(r'\n\n',data)
+		articleTitle = str(data[0]).split("/")[-1]
+		articleTitle = articleTitle.split("-")
+		articleTitle[-1] = articleTitle[-1].split(".")[0]
+		title = defaultdict(int)
+		for t in articleTitle:
+			title[t] = 1
+		articleData = data[1]
+		articleSummary = data[2]
 		entityMapping = data[3]	
 		
-	
-	featureProb = calcFeatureProb(totalSentences)
-	return featureProb
-
-
+		vocabWordFreq,istitle,totalSentences,sentLabel = splitSentLabel(articleData,title)
+		totalSentences += ttotalSentences
+		sentTFIDF = calcTFIDF(vocabWordFreq,totalSentences,sentLabel)
+		testSentFeature = extractTestFeature(sentTFIDF,istitle)	
+		predictedProbLabel = naiveBayesClassifier(testSentFeature,featureProb,probLabel0,probLabel1,probLabel2)
+		checkAccuracy(predictedLabel,sentLabel,totalSentences)
 
 def main():
-	featureProb = traindata()
-		
-	
+	featureProb,probLabel0,probLabel1,probLabel2 = traindata()
+	testdata(featureProb,probLabel0,probLabel1,probLabel2)
+	print("Accuracy of Model: ", accuracy)
 	
 
 if __name__=="__main__":
